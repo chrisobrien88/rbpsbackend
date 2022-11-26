8b8b33a616e4b4d83ce6bf342fd08dd401a8017d
Ben MacAndrews <ben.macandrews@gmail.com>
2022-11-26 14:36:44 +0000

    feat: styles ScoreInputForm

--- CRUD-MERN/client/tailwind.config.js
+++ CRUD-MERN/client/tailwind.config.js
@@ -3,7 +3,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ["./public/index.html"],
+  content: ["./public/index.html","./src/components/Input.js","./src/components/ScoreInputForm.js"],
   theme: {
     extend: {
       fontFamily: {
